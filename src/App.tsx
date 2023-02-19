d15f747752805d5da5c1d426113b1c51a13d420b
zaki <zaki20020302@gmail.com>
2023-02-20 08:48:54 +0900

    test: #5 型未指定によるバグの確認

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import axios from "axios";
+import { useState } from "react";
+import { List } from "./List";
 
 export default function App() {
+  const [lists, setLists] = useState<any>([]);
   const onClickFetchData = () => {
     axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      console.log(res);
+      setLists(res.data);
     });
   };
   return (
     <div>
       <button onClick={onClickFetchData}>データ取得</button>
+      {lists.map((list) => (
+        <List title={list.title} userId={list.userid} />
+      ))}
     </div>
   );
 }
