32ef8dde418421dcc62333acc147ec7932634ea3
zaki <zaki20020302@gmail.com>
2023-02-20 08:22:25 +0900

    feat: #3 axiosによるデータ取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import axios from "axios";
+
 export default function App() {
-  return <></>;
+  const onClickFetchData = () => {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      console.log(res);
+    });
+  };
+  return (
+    <div>
+      <button onClick={onClickFetchData}>データ取得</button>
+    </div>
+  );
 }
